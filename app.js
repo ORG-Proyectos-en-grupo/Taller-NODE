18f5f37db14b613fa8747aa9bf2d0a00680230ec
Hermis999 <hermisjulian999@gmail.com>
2024-12-13 10:18:52 -0500

    agregado

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/userRoutes');
+
+require('./config/db');
+
+// Configurar middleware 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set('view engine', 'ejs');
+
+// rutas
+app.use('/', userRoutes);
+
+// Puerto de la aplicación
+app.listen(3000, ()=> {
+    console.log('Servidor corriendo en http://localhost:3000');
+})
---
+++ config/db.js
@@ -0,0 +1,5 @@
+/* Configurar la conexión a la base de datos MongoDB*/
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/tuBaseDeDatos');
